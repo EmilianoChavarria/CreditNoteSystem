ec734385ab3e86210d27c5ce00fa90cd4c0fea9c
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-19 14:32:39 -0600

    feat: add CustomerService and Customer interface for customer management

---
+++ src/app/core/services/customer-service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http-service';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
+import { Customer } from '../../data/interfaces/Customer';
+import { ApiResponse } from '../../data/interfaces/ApiResponse-interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(
+    private _httpService: HttpService
+  ) { }
+
+  getCustomers(): Observable<Customer[]> {
+    return this._httpService.get<Customer[]>('/customers').pipe(
+      tap((response: ApiResponse<Customer[]>) => {
+        if (response.success) {
+          // console.log(response);
+        }
+      }),
+      map((response: ApiResponse<Customer[]>) => response.data ?? []),
+      catchError(error => {
+        console.log(error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+}
