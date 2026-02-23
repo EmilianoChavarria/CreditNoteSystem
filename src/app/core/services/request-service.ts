c08f9964a5c099791c20e37c86dcec3c55a12900
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-23 16:22:47 -0600

    feat: implement request retrieval by type in RequestService

--- src/app/core/services/request-service.ts
+++ src/app/core/services/request-service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http-service';
+import { catchError, map, Observable, tap } from 'rxjs';
+import { Request } from '../../data/interfaces/Request';
+import { ApiResponse } from '../../data/interfaces/ApiResponse-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor() { }
+  constructor(
+    private _httpService: HttpService
+  ) { }
+
+  getRequestsByType(id: number): Observable<Request[]> {
+    return this._httpService.get<Request[]>(`/requests/${id}`).pipe(
+      tap((response: ApiResponse<Request[]>) => {
+        if (response.success) {
+          // console.log(response);
+        }
+      }),
+      map((response: ApiResponse<Request[]>) => response.data ?? []),
+      catchError(error => {
+        console.log(error);
+        throw error;
+      })
+    )
+  }
 
 }
