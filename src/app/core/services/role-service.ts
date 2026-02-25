a091d0e2a652ee4addb23d834924c39e5685ed7a
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-25 12:50:56 -0600

    feat: add AssignPermission and RolePermission interfaces to RoleService

--- src/app/core/services/role-service.ts
+++ src/app/core/services/role-service.ts
@@ -3,6 +3,22 @@ import { HttpService } from './http-service';
 import { catchError, map, Observable, tap } from 'rxjs';
 import { Role } from '../../data/interfaces/User';
 import { ApiResponse } from '../../data/interfaces/ApiResponse-interface';
+import { RequestType } from '../../data/interfaces/Request';
+
+export interface AssignPermission {
+  requestType: RequestType;
+  role: Role;
+  hasAccess: boolean;
+}
+
+export interface RolePermission {
+  id: number;
+  roleId: number;
+  requestTypeId: number;
+  hasAccess: boolean;
+  requesttype: RequestType;
+  role: Role;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +38,30 @@ export class RoleService {
       }),
       map((response: ApiResponse<Role[]>) => response.data ?? []),
       catchError(error => {
+        console.log(error);
+        throw error;
+      })
+    )
+  }
+
+  getAllPermissions(): Observable<RolePermission[]> {
+    return this._httpSevice.get<RolePermission[]>('/rolesPermission').pipe(
+      map((response: ApiResponse<RolePermission[]>) => response.data ?? []),
+      catchError(error => {
+        console.log(error);
+        throw error;
+      })
+    )
+  }
+
+  assignPermission(data: { roleId: number, requestTypeId: number, hasAccess: boolean }) {
+    return this._httpSevice.post('/rolesPermission/assign', data).pipe(
+      tap((response) => {
+        if (response.success) {
+
+        }
+      }),
+      catchError((error) => {
         console.log(error);
         throw error;
       })
