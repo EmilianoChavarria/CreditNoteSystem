d0e1d80111c6892c4b6c058c9fc0dff4023adf0e
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-26 10:25:07 -0600

    fix: standardize formatting in User and Role interfaces

--- src/app/data/interfaces/User.ts
+++ src/app/data/interfaces/User.ts
@@ -1,18 +1,23 @@
 export interface User {
-  id: number
-  fullName: string
-  email: string
-  roleId: number
-  supervisorId: any
-  preferredLanguage: string
-  isActive: boolean
-  deletedAt: string
-  createdAt: string
-  updatedAt: string
-  role?: Role
+  id: number;
+  fullName: string;
+  email: string;
+  roleId: number;
+  supervisorId: any;
+  preferredLanguage: string;
+  isActive: boolean;
+  deletedAt: string;
+  createdAt: string;
+  updatedAt: string;
+  role?: Role;
 }
 
 export interface Role {
-  id: number
-  roleName: string
+  id: number;
+  roleName: string;
+  color?: string;
+  isActive?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  deletedAt?: string;
 }
