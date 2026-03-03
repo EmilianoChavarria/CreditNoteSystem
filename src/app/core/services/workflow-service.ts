f2bfd1442239d2c75c797f8b8211043ca58dd264
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-03 17:50:00 -0600

    feat: implement WorkflowService with methods for retrieving workflows and classification types

---
+++ src/app/core/services/workflow-service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http-service';
+import { Workflow } from '../../data/interfaces/Workflow';
+import { catchError, map, tap } from 'rxjs';
+import { ApiResponse } from '../../data/interfaces/ApiResponse-interface';
+
+export interface ClassificationTypeGroup {
+  type: string | null;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorkflowService {
+
+  constructor(
+    private _httpService: HttpService
+  ) { }
+
+  getWorkflows() {
+    return this._httpService.get<Workflow[]>('/workflows').pipe(
+      tap((response: ApiResponse<Workflow[]>) => {
+        if (response.success) {
+
+        }
+      }),
+      map((response: ApiResponse<Workflow[]>) => response.data ?? []),
+      catchError((error) => {
+        console.log(error);
+        throw error;
+      })
+    )
+  }
+
+  getClassificationTypes() {
+    return this._httpService.get<ClassificationTypeGroup[]>('/classifications/grouped').pipe(
+      tap((response: ApiResponse<ClassificationTypeGroup[]>) => {
+        if (response.success) { }
+
+      }),
+      map((response: ApiResponse<ClassificationTypeGroup[]>) => response.data ?? []),
+      catchError((error) => {
+        console.log(error);
+        throw error;
+      })
+    )
+  }
+
+}
