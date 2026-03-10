56003fe385e275cce5e9325c2e862641210f0b24
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-10 15:57:03 -0600

    feat: add MyApprovals component with HTML and CSS files

---
+++ src/app/features/pages/my-approvals/my-approvals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-approvals',
+  imports: [],
+  templateUrl: './my-approvals.html',
+  styleUrl: './my-approvals.css',
+})
+export class MyApprovals {
+
+}
