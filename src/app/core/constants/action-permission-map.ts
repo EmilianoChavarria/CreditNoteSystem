880be91f31456f9e29348f863c68027bd0c58b61
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-02 12:58:58 -0600

    feat: add missing action permissions for see_history and see_attachments

--- src/app/core/constants/action-permission-map.ts
+++ src/app/core/constants/action-permission-map.ts
@@ -3,6 +3,8 @@ export const CUSTOM_ACTION_PERMISSION_SLUG_MAP: Record<string, readonly string[]
   decline: ['decline', 'reject', 'rejected'],
   pdf: ['pdf', 'download_pdf'],
   edit: ['edit'],
+  see_history: ['history', 'view_history', 'see_history'],
+  see_attachments: ['see_attachments', 'see_attachment', 'see_atachments', 'see_atachment', 'view_attachments', 'view_attachment'],
   history: ['history', 'view_history'],
   delete: ['delete'],
 };
