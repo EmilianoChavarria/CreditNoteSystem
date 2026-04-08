c0938ef470dd1237f521b786a8516cc33eb8d5a7
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-08 14:55:27 -0600

    feat: update stat card component to use dynamic title, value, and icon

--- src/app/features/dashboard/components/stat-card/stat-card.ts
+++ src/app/features/dashboard/components/stat-card/stat-card.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
     selector: 'app-stat-card',
     templateUrl: './stat-card.html',
     styleUrl: './stat-card.css',
+        changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [LucideAngularModule],
 })
 export class StatCard {
-
+    readonly title = input<string>('Started');
+    readonly value = input<string>('07');
+    readonly iconName = input<string>('calendar-clock');
 }
