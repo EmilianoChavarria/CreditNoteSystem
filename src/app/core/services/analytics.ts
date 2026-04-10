832882612d76e62dab9ce4929c18f434c8f8e883
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-10 09:06:49 -0600

    feat: integrate Google Analytics tracking with AnalyticsService and update index.html

---
+++ src/app/core/services/analytics.ts
@@ -0,0 +1,29 @@
+// src/app/services/analytics.service.ts
+import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag: Function; // Declara la función global de GA
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyticsService {
+  constructor(private router: Router) {}
+
+  public init() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Registra la vista de página manualmente
+      gtag('config', 'G-Y5P774SRZP', {
+        page_path: event.urlAfterRedirects
+      });
+    });
+  }
+
+  // Método para eventos personalizados (clics, conversiones, etc.)
+  public trackEvent(eventName: string, params: object) {
+    gtag('event', eventName, params);
+  }
+}
