6fed471e24394764fd65001a27b428d992e8f37e
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-13 16:14:19 -0600

    feat: add bulk uploaded files table component with file management features

---
+++ src/app/features/requests/components/batchs/shared/bulk-uploaded-files-table/bulk-uploaded-files-table.ts
@@ -0,0 +1,25 @@
+import { Component, input, output } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+interface UploadedFileRow {
+  name: string;
+  sizeLabel: string;
+  type: string;
+  uploadedAt: string;
+}
+
+@Component({
+  selector: 'app-bulk-uploaded-files-table',
+  imports: [TranslatePipe, LucideAngularModule],
+  templateUrl: './bulk-uploaded-files-table.html',
+})
+export class BulkUploadedFilesTable {
+  rows = input.required<UploadedFileRow[]>();
+  caption = input<string>('Listado de archivos cargados');
+  remove = output<number>();
+
+  onRemove(index: number): void {
+    this.remove.emit(index);
+  }
+}
