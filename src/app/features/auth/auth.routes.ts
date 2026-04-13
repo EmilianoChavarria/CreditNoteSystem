4989ec855f66a5c946de45ccdf20ac03396930ec
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-13 16:03:00 -0600

    fix: remove unused LoginGuard import from auth.routes.ts

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Layout } from './pages/layout/layout';
-import { LoginGuard } from '../../core/guards/login.guard';
 import { Login } from './pages/login/login';
 
 
