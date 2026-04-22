a850c627f8d12f0c1caccea1e888eb576372ea85
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-22 16:00:26 -0600

    feat: restructure layout component to include navbar and footer for improved page structure

--- src/app/features/auth/pages/layout/layout.ts
+++ src/app/features/auth/pages/layout/layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../../components/navbar/navbar';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
     selector: 'app-layout',
     templateUrl: './layout.html',
     styleUrl: './layout.css',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, Navbar, Footer],
 })
 export class Layout {
 
