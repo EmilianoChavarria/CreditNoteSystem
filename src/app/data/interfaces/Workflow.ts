49a1875c68143e5183051c97f5fcb9e44c479a76
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-05-06 17:45:55 -0600

    feat(workflow): remove name and description fields from Workflow interface

--- src/app/data/interfaces/Workflow.ts
+++ src/app/data/interfaces/Workflow.ts
@@ -2,15 +2,13 @@ import { Classification, RequestType } from "./Request"
 
 export interface Workflow {
   id?: number;
-  name: string;
-  description: string;
   isActive?: boolean;
   requestTypeId: number;
   classificationType?: string;
   createdAt?: string;
   updatedAt?: string;
   deletedAt?: string;
-  request_type?: RequestType;
+  requestType?: RequestType;
   classification?: Classification;
   steps?: WorkflowStep[];
 }
