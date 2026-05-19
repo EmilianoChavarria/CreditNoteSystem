20d0c6487647797592b8ff9dd32d1996d3b75a58
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-05-19 17:34:39 -0600

    feat(interfaces): update Customer manager fields to object type, add CustomerLocalPayload, add equivalentRoleId to Role

--- src/app/data/interfaces/Customer.ts
+++ src/app/data/interfaces/Customer.ts
@@ -20,28 +20,35 @@ export interface Customer {
   moneda: string;
   idUsuarioCc: string;
   ulActualizacionCc: string;
-  customer?: CustomerLocal;
+  clienteExt?: CustomerLocal;
 }
 
 export interface CustomerLocal {
   idCustomer?: number;
   idCliente?: string;
+  area: string | null;
+  salesEngineerId?: Manager | null;
+  salesManagerId?: Manager | null;
+  financeManagerId?: Manager | null;
+  marketingManagerId?: Manager | null;
+  customerServiceManagerId?: Manager | null;
+  distributor?: Manager | null;
+}
+
+export interface Manager {
+  id: number;
+  fullName: string;
+  role?: { roleName: string };
+}
+
+export interface CustomerLocalPayload {
+  idClient?: string;
   area: string;
   salesEngineerId: string;
   salesManagerId: string;
   financeManagerId: string;
   marketingManagerId: string;
   customerServiceManagerId: string;
-  salesEngineer?: Manager;
-  salesManager?: Manager;
-  financeManager?: Manager;
-  marketingManager?: Manager;
-  customerServiceManager?: Manager;
-}
-
-export interface Manager {
-  id: number
-  fullName: string
 }
 
 
