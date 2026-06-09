4f95eeef1597f3140cf867f4c4714f12927da917
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-06-09 15:11:41 -0600

    feat(customer-info-modal): replace hardcoded strings with translate pipe

--- src/app/features/settings/pages/customers/components/customer-info-modal/customer-info-modal.ts
+++ src/app/features/settings/pages/customers/components/customer-info-modal/customer-info-modal.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Modal } from '../../../../../../shared/components/ui/modal/modal';
 import { Customer } from '../../../../../../data/interfaces/Customer';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-customer-info-modal',
-  imports: [Modal],
+  imports: [Modal, TranslatePipe],
   templateUrl: './customer-info-modal.html',
 })
 export class CustomerInfoModal {
