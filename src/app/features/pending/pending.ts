5a8e64bec726a9a3838bec9e9f19c7b1fface17d
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-06-11 17:43:10 -0600

    feat(pending): add totalAmount column and improve request-info-modal

--- src/app/features/pending/pending.ts
+++ src/app/features/pending/pending.ts
@@ -4,7 +4,7 @@ import { AccionPersonalizada, Column, Table } from '../../shared/components/ui/t
 import { Spinner } from '../../shared/components/ui/spinner/spinner';
 import moment from 'moment';
 import { Badge } from '../../shared/components/ui/badge/badge';
-import { SlicePipe, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, SlicePipe, UpperCasePipe } from '@angular/common';
 import { Modal } from '../../shared/components/ui/modal/modal';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { WorkflowHistoryDrawer } from '../history/components/workflow-history-drawer/workflow-history-drawer';
@@ -21,7 +21,7 @@ import { RequestListBase } from '../../shared/base/request-list.base';
     selector: 'app-pending',
     templateUrl: './pending.html',
     styleUrl: './pending.css',
-    imports: [TranslatePipe, Table, Spinner, Badge, UpperCasePipe, Modal, WorkflowHistoryDrawer, WorkflowHistoryModal, PendingAttachmentsModal, RequestInfoModal, ReactiveFormsModule, SlicePipe]
+    imports: [TranslatePipe, Table, Spinner, Badge, UpperCasePipe, CurrencyPipe, Modal, WorkflowHistoryDrawer, WorkflowHistoryModal, PendingAttachmentsModal, RequestInfoModal, ReactiveFormsModule, SlicePipe]
 })
 export class Pending extends RequestListBase {
     private readonly _roleService = inject(RoleService);
@@ -44,6 +44,7 @@ export class Pending extends RequestListBase {
             key: 'createdAt', label: 'PENDING_PAGE.CREATED_AT', sortable: true,
             render: (value) => value ? moment(value).format('DD/MM/YYYY HH:mm:ss') : '-'
         },
+        { key: 'totalAmount', label: 'PENDING_PAGE.AMOUNT', sortable: false, customTemplate: true },
         { key: 'user.fullName', label: 'CREATED BY', sortable: true}
     ];
 
