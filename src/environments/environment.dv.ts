ffd463c37a37275b7d4fe9f063ed9cf08f072b43
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-07-13 14:43:55 -0600

    feat(environments): add environment configuration files for development, production, and QA

---
+++ src/environments/environment.dv.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://dv.timken.ittec.mx/notasCreditos_back/public/api',
+  socketHost: 'dv.timken.ittec.mx',
+  socketPort: 8080,
+  pusherKey: '2d14276d43635db2b0cd',
+  pusherCluster: 'us2',
+};
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://qa.timken.ittec.mx/notasCreditos_back/public/api',
+  socketHost: 'qa.timken.ittec.mx',
+  socketPort: 8080,
+  pusherKey: '2d14276d43635db2b0cd',
+  pusherCluster: 'us2',
+};
