e7a097d2dfb01e7a691af39db6a2b253eb06451c
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-09-22 09:09:46 -0600

    refactor(customer-interface): rename correosDevoluciones to correosForecast

--- src/app/data/interfaces/Customer.ts
+++ src/app/data/interfaces/Customer.ts
@@ -35,7 +35,7 @@ export interface CustomerLocal {
   marketingManagerId?: Manager | null;
   customerServiceManagerId?: Manager | null;
   distributor?: Manager | null;
-  correosDevoluciones?: string | null;
+  correosForecast?: string | null;
 }
 
 export interface Manager {
